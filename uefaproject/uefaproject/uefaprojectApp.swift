9b42386574dc9295b7000743f27c760073b46eb7
SebastianCatur <catur_sebastian@yahoo.com>
2024-10-18 10:13:50 +0300

    first commit creating the project

---
+++ uefaproject/uefaproject/uefaprojectApp.swift
@@ -0,0 +1,17 @@
+//
+//  uefaprojectApp.swift
+//  uefaproject
+//
+//  Created by Sebastian Catur on 18.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct uefaprojectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
